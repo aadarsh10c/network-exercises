0a208bb72c7089be15e960a274809b4bea1f7ce7
Adarsh Kumar <adarsh010c@gmail.com>
2020-12-19 00:33:14 +0530

    retrieve-image-urllib modifed
    Extract the name of the file from url

--- retrieve-image-urllib.py
+++ retrieve-image-urllib.py
@@ -3,11 +3,15 @@
 import urllib.request
 
 host='http://data.pr4e.org/cover3.jpg'
-newHost='https://i2.wp.com/ramenswag.com/wp-content/uploads/2016/07/sasuke_wallpaper_4ce23.jpg?resize=300%2C188&ssl=1'
+#newHost='https://i2.wp.com/ramenswag.com/wp-content/uploads/2016/07/sasuke_wallpaper_4ce23.jpg?resize=300%2C188&ssl=1'
 
-data=urllib.request.urlopen(newHost)
+#Extract the  name of the image
+fileName = host.split('/')[-1]
 
-with open('stuff2.jpg','wb') as f:
+#download the image
+data=urllib.request.urlopen(host)
+
+with open(fileName,'wb') as f:
     while True:
         image=data.read(100000)
         if len(image) < 1: break
